1146331c1b5b000db7fd727b2e982bfa4c056954
SKajiwara <gold.shin789@gmail.com>
2021-03-10 10:36:31 -0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+      </div>
+    );
+  }
+}
