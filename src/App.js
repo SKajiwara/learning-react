2f9bd969b579b9a8c831be15612a672ebf18307a
SKajiwara <gold.shin789@gmail.com>
2021-03-10 16:57:15 -0700

    Delete Button working. State tutorial.

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Table from "./Table.js";
+
+class App extends Component {
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
+
+  state = {
+    characters: [
+      {
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="Container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+      </div>
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App.js";
 import "./style.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1> Hello World </h1>
-      </div>
-    );
-  }
-}
+ReactDOM.render(<App />, document.getElementById("root"));
